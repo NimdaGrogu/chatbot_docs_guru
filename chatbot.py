e56d1d10e095a3ccab21a5f91cc4074cbecb654e
jonathan <jonathan@nimda.sh>
2024-02-07 15:13:10 -0600

    Multipage app

--- chatbot.py
+++ chatbot.py
@@ -31,29 +31,6 @@ def main():
     if user_question := st.chat_input(placeholder="Can you give me a detail summary?"):
         handle_userinput(user_question)
 
-    with st.sidebar:
-        st.sidebar.header("Knowledge Base :open_book:")
-
-        st.subheader("From PDF's :books:")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Loading..."):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)[0]
-
-                # create vector store
-                vs = VectorStore()
-                vectorstore = vs.build_knowledgebase_faiss(text_chunks=text_chunks, save=True)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-            st.success("Done!")
-
 
 if __name__ == '__main__':
     main()
---
+++ pages/upload_files.py
@@ -0,0 +1,43 @@
+from VectordbOps import VectorStore
+from CorpusTextOps import get_text_chunks, get_pdf_text
+from UserImput import get_conversation_chain
+import streamlit as st
+from htmlTemplates import css
+import logging
+
+
+def main():
+    logger = logging.getLogger("ChatBot")
+    # Set the logging level to INFO
+    logger.setLevel(logging.INFO)
+    st.set_page_config(page_title="Chatbot",
+                       page_icon="🤖",
+                       layout="centered",
+                       initial_sidebar_state="collapsed")
+    st.write(css, unsafe_allow_html=True)
+
+    st.header("Knowledge Base :open_book:")
+
+    st.subheader("From PDF's :books:")
+    pdf_docs = st.file_uploader(
+        "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    if st.button("Process"):
+        with st.spinner("Loading..."):
+            # get pdf text
+            raw_text = get_pdf_text(pdf_docs)
+
+            # get the text chunks
+            text_chunks = get_text_chunks(raw_text)[0]
+
+            # create vector store
+            vs = VectorStore()
+            vectorstore = vs.build_knowledgebase_faiss(text_chunks=text_chunks, save=True)
+
+            # create conversation chain
+            st.session_state.conversation = get_conversation_chain(
+                vectorstore)
+        st.success("Done!")
+
+
+if __name__ == '__main__':
+    main()
